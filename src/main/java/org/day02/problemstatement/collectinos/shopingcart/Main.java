66d38381c9e8add25cdd2fa7d8fd62eb848952e0
sachin shukla <sachinshukla232003@gmail.com>
2025-02-13 01:16:53 +0530

    [Sachin Shukla] Add.shoping cart system

---
+++ src/main/java/org/day02/problemstatement/collectinos/shopingcart/Main.java
@@ -0,0 +1,20 @@
+package org.day02.problemstatement.collectinos.shopingcart;
+
+
+public class Main {
+    public static void main(String[] args) {
+        CartManager manager = new CartManager();
+        manager.addProduct("Laptop", 800.00);
+        manager.addProduct("Phone", 500.00);
+        manager.addProduct("Headphones", 100.00);
+
+
+        manager.addToCart("Phone");
+        manager.addToCart("Laptop");
+        manager.addToCart("Headphones");
+        manager.addToCart("custom");
+        System.out.println("-------------------------");
+
+        CartDisplay.displaySortedCart(manager.getCart());
+    }
+}
