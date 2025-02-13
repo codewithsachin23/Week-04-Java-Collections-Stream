c69d43df0f823952e9862fb63294b2a67e2e0675
sachin shukla <sachinshukla232003@gmail.com>
2025-02-13 16:36:34 +0530

    [Sachin Shukla] Add.convert Lower Case

---
+++ src/main/java/org/day03/problemstatement/streams/convertuppercasetolowercase/Main.java
@@ -0,0 +1,13 @@
+package org.day03.problemstatement.streams.convertuppercasetolowercase;
+
+
+public class Main {
+        public static void main(String[] args) {
+            String filePath="convertLowerCase.txt";
+            String writerHere="convertUpperCase.txt";
+            ConvertLowerCase.convertLowerCase(filePath,writerHere);
+
+        }
+
+
+}
---
+++ src/test/java/day03/streamstesting/convertLowerCaseTest.java
@@ -0,0 +1,4 @@
+package day03.streamstesting;
+
+public class convertLowerCaseTest {
+}
