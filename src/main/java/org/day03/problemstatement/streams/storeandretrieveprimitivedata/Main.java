67a3abaa35a4585e13a96111dcd780d5de00db5d
sachin shukla <sachinshukla232003@gmail.com>
2025-02-13 16:39:31 +0530

    [Sachin Shukla] Add. Data Stream

---
+++ src/main/java/org/day03/problemstatement/streams/storeandretrieveprimitivedata/Main.java
@@ -0,0 +1,9 @@
+package org.day03.problemstatement.streams.storeandretrieveprimitivedata;
+
+public class Main {
+    public static void main(String[] args) {
+        DataStreams.storeStudentDetails();
+
+        DataStreams.retrieveStudentDetails();
+    }
+}
