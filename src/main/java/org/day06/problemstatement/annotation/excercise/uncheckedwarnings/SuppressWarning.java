6ea7c9da967b0a6a1cbb03ac0d29e7afb974f750
sachin shukla <sachinshukla232003@gmail.com>
2025-02-16 21:33:14 +0530

    [Sachin Shukla] Add.unchecked warnings

---
+++ src/main/java/org/day06/problemstatement/annotation/excercise/uncheckedwarnings/SuppressWarning.java
@@ -0,0 +1,17 @@
+package org.day06.problemstatement.annotation.excercise.uncheckedwarnings;
+
+import java.util.ArrayList;
+
+public class SuppressWarning {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        //Creating an ArrayList without generics
+        ArrayList list = new ArrayList();
+        //Adding elements to the list
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        //Printing the list
+        System.out.println(list);
+    }
+}
