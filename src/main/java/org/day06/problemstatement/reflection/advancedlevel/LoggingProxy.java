b20d67c44a7ad8718c8e79cc3c33dc4e16eb4b3b
sachin shukla <sachinshukla232003@gmail.com>
2025-02-16 21:56:57 +0530

    [Sachin Shukla] Add.Logging proxy

---
+++ src/main/java/org/day06/problemstatement/reflection/advancedlevel/LoggingProxy.java
@@ -0,0 +1,43 @@
+package org.day06.problemstatement.reflection.advancedlevel;
+
+import java.lang.reflect.*;
+
+interface Greeting {
+    void sayHello();
+}
+
+class Hello implements Greeting {
+    public void sayHello() {
+        System.out.println("Hello, World!");
+    }
+}
+
+class LoggingInvocationHandler implements InvocationHandler {
+    private final Object target;
+
+    public LoggingInvocationHandler(Object target) {
+        this.target = target;
+    }
+
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("Invoking method: " + method.getName());
+        return method.invoke(target, args);
+    }
+}
+
+public class LoggingProxyExample {
+    public static void main(String[] args) {
+        Greeting original = new Hello();
+
+        // Creating a dynamic proxy instance
+        Greeting proxyInstance = (Greeting) Proxy.newProxyInstance(
+                Hello.class.getClassLoader(),
+                new Class[]{Greeting.class},
+                new LoggingInvocationHandler(original)
+        );
+
+        // Calling method through proxy
+        proxyInstance.sayHello();
+    }
+}
