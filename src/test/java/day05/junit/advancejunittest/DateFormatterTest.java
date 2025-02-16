75396efdb669c5c7b8d50744a7c812db50ed4da6
sachin shukla <sachinshukla232003@gmail.com>
2025-02-16 20:57:52 +0530

    [Sachin Shukla] Add. date format and testing

---
+++ src/test/java/day05/junit/advancejunittest/DateFormatterTest.java
@@ -0,0 +1,19 @@
+package day05.junit.advancejunittest;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.day05.problemstatement.junit.advancedJUnit.testingdateformatte.DateFormatter;
+import org.junit.jupiter.api.Test;
+public class DateFormatterTest {
+    private final DateFormatter dateFormatter= new DateFormatter();
+
+    @Test
+    void testValidDateFormat(){
+        assertEquals("15-02-2025",dateFormatter.formatDate("2025-02-15"));
+        assertEquals("23-09-1999",dateFormatter.formatDate("1999-09-23"));
+    }
+    @Test
+    void testInvaildDateFormat(){
+        assertThrows(IllegalArgumentException.class, () -> dateFormatter.formatDate("15-02-2025"));
+        assertThrows(IllegalArgumentException.class, () -> dateFormatter.formatDate("2025/02/15"));
+    }
+}
